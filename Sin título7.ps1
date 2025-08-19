852cfc88f0a524241a00a29cd76a1cc635e3cbde
Rodrigoperez14 <rodrigo.perezgn@uanl.edu.mx>
2025-08-19 11:43:45 -0600

    Rename Sin título7.ps1 to Manifiestoam.ps1

--- Sin título7.ps1
+++
@@ -1,1 +0,0 @@
-﻿New-ModuleManifest -Path '.\AuditoriaBasica.psd1' -RootModule 'AuditoriaBasica.psm1' -ModuleVersion '1.0.0' -Author 'Equipo de Ciberseguridad' -Description 'Módulo para auditoría básica de usuarios y servicios' -PowerShellVersion '5.1' 
